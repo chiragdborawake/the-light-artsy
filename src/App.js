65468ee7d7adb243f6cb87c9a8acdf6567413bc4
chiragdborawake <chiragdborawake@gmail.com>
2021-12-25 22:36:46 +0530

    Scroll to top feature added.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import LightArtsy from './LightArtsy'
 import { BrowserRouter } from 'react-router-dom';
-
+import ScrollToTop from './ScrollToTop'
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <ScrollToTop />
         <LightArtsy></LightArtsy>
       </BrowserRouter>
     </div >
