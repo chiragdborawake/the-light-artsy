1af66a3472d8ce2293c46a3a5b15bdb99c436204
chiragdborawake <chiragdborawake@gmail.com>
2021-12-25 16:36:14 +0530

    Table lamps featuer added

--- src/routes/tableLamps.js
+++ src/routes/tableLamps.js
@@ -1,7 +1,8 @@
 import { useEffect } from 'react';
+import TableLamp from '../TableLamp';
 export default function TableLamps() {
   useEffect(() => {
     document.title = "Table Lamps - The Light Artsy"
   }, [])
-  return (<div>TableLamps</div>)
+  return (<div><TableLamp /></div>)
 } 
