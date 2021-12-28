e9598570c0c55fa19763068ce13f8b8a3901583a
chiragdborawake <65819096+chiragdborawake@users.noreply.github.com>
2021-12-28 19:27:05 +0530

    Feature/string lights (#22)
    * String lights feature added.
    
    * Prettier formating

--- src/routes/stringLight.js
+++ src/routes/stringLight.js
@@ -1,7 +1,12 @@
 import { useEffect } from "react";
-export default function StringLight() {
+import StringLights from "../StringLights";
+export default function StringLightRoute() {
   useEffect(() => {
     document.title = "String Lihgts - The Light Artsy";
   }, []);
-  return <div>StringLight</div>;
+  return (
+    <div>
+      <StringLights />
+    </div>
+  );
 }
